1ccfe09fa5240b9fdb9c9a2334e1c24793f71087
dheena <dheenadhayalanr19@gmail.com>
2022-02-17 22:00:21 +0530

    Create appropriate progress bar class

--- helper.py
+++ helper.py
@@ -1,5 +1,12 @@
 from datetime import datetime
 import time
+from tqdm import tqdm
+
+
+class ProgressBar(tqdm):
+    def new_update(self, received, total):
+        self.total = total
+        self.update(received-self.n)
 
 
 def wait_till(target):
